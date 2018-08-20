fac746000fe6735124f31f111a8fea42fed9f2f1
will <wherding@asu.edu>
2018-08-19 22:06:06 -0700

    pushing this to github so i can work on it from another computer

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.CharSelection').css("background-color","blue");
+var char = document.createElement("img");
+//console.log(char);
+//char.attr("src") = "../img/2-StarWars.jpg";
+$('.CharSelection1').attr('src',"./assets/javascript/2-StarWars.jpg");// = "../img/2-StarWars.jpg";
+console.log($('CharSelection1'));
+   // $('CharSelection').appendChild(char);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
