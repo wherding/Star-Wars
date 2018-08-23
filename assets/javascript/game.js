88d4b91a338d0c20781a0ddb6ae993a21144a1d2
William Herding <William@corp.cableone.net>
2018-08-22 18:40:23 -0700

    worked on attack button. need better way of accesing object data.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,60 +2,57 @@
 var loaded = 0;
 
 var imgs = [];
+var defender;
+var you;
 $(document).ready(function(){
     loadOnce();
 
-//begin loaded into my loadonce()
-/*
-//loop to populate selection div
-chars.forEach(x => {
-    var newImg = $("<img>" )//document.createElement("img");
-$(newImg).attr("src", x.img);// = chars[0].img;
-$(newImg).addClass(x.class)
-$(newImg).attr("data-name",x.name)
-$('.CharSelection1').append(newImg);
-imgs.push(newImg)
-});
-*/
-//end coppied into my load once
-/*
-var newImg = document.createElement("img");
-$(newImg).attr("src", chars[0].img);// = chars[0].img;
-$(newImg).addClass(chars[0].name)
-console.log(newImg);
-    $('.CharSelection').css("background-color","blue","font-color","white");
-  */ 
- 
-//click event to select char.. need to improve so that it moves others to enemy div
-
-
-
-
-   // $('CharSelection').appendChild(char);
-return;
-});
 
 
 function loadOnce(){
     if(loaded <=0){
         //begin object array
         var chars = [
-            obi = {name:"obi-won Kenobi", img:"./assets/img/obi.jpg"},
-            luke = {name:"Luke Skywalker", img:"./assets/img/Luke.png"},
-            sid = {name:"Darth Sidious", img:"./assets/img/sidious.png"},
-            mal = {name:"Darth Maul",  img:"./assets/img/Maul.jpeg"}
+            obi = {name:"obi-won Kenobi", img:"./assets/img/obi.jpg", hp:"120",damage:"8",counter:"25"},
+            luke = {name:"Luke Skywalker", img:"./assets/img/Luke.png", hp:"200",damage:"8",counter:"25"},
+            sid = {name:"Darth Sidious", img:"./assets/img/sidious.png",hp:"150",damage:"8",counter:"25"},
+            mal = {name:"Darth Maul",  img:"./assets/img/Maul.jpeg",hp:"180",damage:"8",counter:"25"}
         ]
 
         //end object array
 
-    
+    //i added alot of stuff to make cards and nwo my click event doesnt work. 
+    //should probably add this info in a more understandable way. 
+    //its not very retravable as it is. 
+    //look into passing the original char object into the imgs array. so i can access it later.
+    //like store the object iin the img somewhere. 
     chars.forEach(x => {
+        var newDIv = $("<div>");
+        $(newDIv).addClass("card");
+        $(newDIv).attr("style","width:200px")
         var newImg = $("<img>" )//document.createElement("img");
     $(newImg).attr("src", x.img);// = chars[0].img;
-    $(newImg).addClass("char")
-    $(newImg).attr("data-name",x.name)
-    $('.CharSelection1').append(newImg);
-    imgs.push(newImg)
+    $(newDIv).addClass("char")
+    $(newImg).addClass("card-img-top")
+    $(newDIv).attr("data-name",x.name)
+    $(newDIv).attr("data-damage",x.damage)
+    var overlayDiv = $("<div>");
+    $(overlayDiv).addClass("card-img-overlay");
+    var overlayText = $("<h4>");
+    $(overlayText).text(x.name);
+    $(overlayText).attr("style","color:red");
+    $(overlayDiv).append(overlayText);
+    var hp = $("<p>");
+    
+    $(hp).text(x.hp);
+    $(hp).addClass("hp");
+    $(hp).attr("style","color:red");
+    $(overlayDiv).append(hp);
+    $(newDIv).append(newImg);
+    $(newDIv).append(overlayDiv);
+   
+    $('.CharSelection1').append(newDIv);
+    imgs.push(newDIv)
     });
     console.log("we are inside of the if statement of hte LoadOnce()");
     loaded++;
@@ -63,15 +60,24 @@ function loadOnce(){
 
 
 
-
-$(document).on("click", ".enemy", function () {
+//start pick defender method
+var die = $(document).on("click", ".enemy", function () {
     $(".Defend").append($(this));
-    console.log("clicked my enemy class method " + $(this).attr("class"));
+    $(this).addClass("defender");
+    defender =  $(event.target)
+    console.log("clicked my enemy class method " + $(this).attr(".hp"));
+    //the line below here returns hp of the defender lets see if i can get him by class
+    console.log("clicked my enemy class method " + $(event.target).children(".hp").text());
+   //as an object?
+   console.log("object defender in enemy method " + $(defender).children(".hp").text());
+    //by class? 
+    console.log("clicked my enemy class method " + $(".defender").children("p.hp").text());
 });
 
 $(document).on("click", ".char", function () {
     //$('img').remove(".Luke");
     var name = $(this).attr("data-name");
+    you = $(event.target);
     //console.log("this is my local name var in my click" + name);
     //console.log("this is my new imgs array " +$(imgs)[0]);
     
@@ -79,6 +85,8 @@ $(document).on("click", ".char", function () {
        // console.log("x.name " +$(x).attr("data-name"));
         if ($(this).attr("data-name")===$(x).attr("data-name")) {
             $('.your').append(x);
+           
+            //console.log(you);
         }
         else{
             $(x).addClass("enemy");
@@ -100,8 +108,17 @@ $(document).on("click", ".char", function () {
 
 });//end .charClick()
 
+//begin attack
+$(".Attack").on("click", function(){
+    console.log("object you in atack method " + $(you).children("p.hp").text()); 
+    console.log($(you).children("p.hp").text());
+    console.log($(you).parent());
+    console.log("object defender in atack method " + $(defender).children(".hp").text());
+    
 
+})
 
+});
 
 
 
